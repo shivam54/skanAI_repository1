d325901466779caf03c5d1b8bb836ab266b90fc7
Shivam Singla <s.aggar1013@gmail.com>
2024-01-26 20:05:11 +0530

    Update manipulate_json.py

--- manipulate_json.py
+++ manipulate_json.py
@@ -1,8 +1,13 @@
 import json
 import datetime
+import os
+
+# Get the absolute path of the script's directory
+script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Read JSON from file
-with open('response.json', 'r') as file:
+response_file_path = os.path.join(script_directory, 'response.json')
+with open(response_file_path, 'r') as file:
     data = json.load(file)
 
 # Manipulate data
@@ -10,7 +15,8 @@ timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 data['title'] = f'Updated Title - {timestamp} shivamData'
 
 # Save manipulated data back to file
-with open('manipulatedResponse.json', 'w') as file:
+manipulated_response_file_path = os.path.join(script_directory, 'manipulatedResponse.json')
+with open(manipulated_response_file_path, 'w') as file:
     json.dump(data, file, indent=2)
 
 # Print manipulated data
